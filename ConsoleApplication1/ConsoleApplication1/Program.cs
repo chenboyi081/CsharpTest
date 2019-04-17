8a96f7f577da5ca9c9f5f46ecdd193d618f7fd78
chenboyi081 <chenboyi081@163.com>
2019-04-17 17:14:26 +0800

    first add

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test;
+
+namespace CsharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            nullable N = new nullable();
+            N.ShowMessage("HELLO WORLD!");
+
+            N.print();
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
---
+++ ConsoleApplication1/Test/nullable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    /// <summary>
+    /// nullable类型测试
+    /// </summary>
+    public class nullable
+    {
+        int? nullIntValue = new Nullable<int>();
+
+        /// <summary>
+        /// 测试显示nullable类型
+        /// </summary>
+        public void print()
+        {
+            int? intTest = 999;
+            Console.WriteLine(intTest.ToString());
+        }
+
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
