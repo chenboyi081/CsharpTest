55dc860700bcb7d0dfc1f41ac03ece76f01fe426
chenboyi081 <chenboyi081@163.com>
2019-04-18 11:14:12 +0800

    add Mylibrary

---
+++ ConsoleApplication1/MyLibrary/EncryptHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass_Library
+{
+    /// <summary>
+    /// 加密帮助类
+    /// </summary>
+    public  class EncryptHelper
+    {
+        #region MD5加密
+        public static string MD5(string pwd)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] data = System.Text.Encoding.Default.GetBytes(pwd);
+            byte[] md5data = md5.ComputeHash(data);
+            md5.Clear();
+            string str = "";
+            for (int i = 0; i < md5data.Length; i++)
+            {
+                str += md5data[i].ToString("x").PadLeft(2, '0');
+
+            }
+            return str;
+        }
+        #endregion
+    }
+}
