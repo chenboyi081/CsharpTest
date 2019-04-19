83feaf89a0613f2f94dfa186d06f7e437ffb7a0f
chenboyi081 <chenboyi081@163.com>
2019-04-20 00:53:22 +0800

    add CsharpBase

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,16 +11,39 @@ namespace CsharpTest
     {
         static void Main(string[] args)
         {
-            StringTest ST = new StringTest();
+            #region 01数据类型和变量
+            //CsharpBase._01VariablesandDatatypes VD = new CsharpBase._01VariablesandDatatypes();
+            //VD.test1();
+            //VD.test2();
+            //VD.longtype();  //求long类型最大和最小值 
+            //VD.decAndFloat();  //decimal,double,float
+            #endregion
 
-            ST.StringTest_operationalcharacter();
-            ST.StringTest_escapeSequence();
-            ST.StringTest_LiteralCharacter();
-            ST.StringTest_statement();
-            ST.StringTest_OrdinalComparison();
-            ST.StringTest_ReferenceEquals();
-            ST.StringTest_IndexOf();
-            ST.StringTest_RegularExpressions();
+            #region 02表达式/占位符转义符等符号/类型转换
+            //CsharpBase._02SymbolsAndExpressions SE = new CsharpBase._02SymbolsAndExpressions();
+            //SE.consoleReadLine();   //从控制台读取输入
+            //SE.escapeSymbol();      //字符串的转义符
+            //SE.placeholder();       //占位符
+            //SE.arithmeticExpression(); //算数表达式
+            //SE.typeConversion02(); //强制类型转换
+            //SE.typeConversion03(); //Parse转换 
+            #endregion
+
+
+
+
+            #region 字符串练习
+            //StringTest ST = new StringTest();
+
+            //ST.StringTest_operationalcharacter();
+            //ST.StringTest_escapeSequence();
+            //ST.StringTest_LiteralCharacter();
+            //ST.StringTest_statement();
+            //ST.StringTest_OrdinalComparison();
+            //ST.StringTest_ReferenceEquals();
+            //ST.StringTest_IndexOf();
+            //ST.StringTest_RegularExpressions(); 
+            #endregion
 
             Console.ReadKey();
         }
