11da2b69169146a54a0ed50661d9bb59bf6f754d
chenboyi081 <chenboyi081@163.com>
2019-05-09 17:05:31 +0800

    添加AutoMapper测试

---
+++ ConsoleApplication1/Test/AutoMapperTest.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Source
+    {
+        public string name { get; set; }
+        public string InfoUrl { get; set; }
+    }
+
+    public class Destination
+    {
+        public string name { get; set; }
+        public string InfoUrl { get; set; }
+        public string aa { get; set; }
+    }
+
+    public class AutoMapperTest
+    {
+        public void cwMap()
+        {
+            Destination des = new Destination()
+            {
+                InfoUrl = "www.cnblogs.com/zaranet",
+                name = "张子浩"
+            };
+            Mapper.Initialize(x => x.CreateMap<Destination, Source>());
+            Source source = AutoMapper.Mapper.Map<Source>(des);
+            Console.WriteLine(source.InfoUrl);
+        }
+
+    }
+}
