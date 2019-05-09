ba5a67a55655acd7b2e674ab0e7dcf749a856f4d
chenboyi081 <chenboyi081@163.com>
2019-05-09 19:05:17 +0800

    add 映射例子

---
+++ ConsoleApplication1/AutoMapperWeb/Common/ATProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using AutoMapperWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoMapperWeb.Common
+{
+    public class ATProfile: Profile
+    {
+        public ATProfile()
+
+        {
+
+            //映射关系配置
+
+            CreateMap<ProductEntity, ProductDTO>();
+            CreateMap<Book, BookViewModel>().ForMember(t => t.Author, s => s.MapFrom(u => u.Author.Name));
+
+        }
+    }
+}
--- ConsoleApplication1/AutoMapperWeb/Controllers/HomeController.cs
+++ ConsoleApplication1/AutoMapperWeb/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using AutoMapperWeb.Common;
+using AutoMapperWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +13,48 @@ namespace AutoMapperWeb.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var productEntity = new ProductEntity() { Name = "Visual Studio 2017", Amount = 500 };
+
+            Book book = new Book()
+            {
+                Title = "Leesin",
+                Author = new Author()
+                {
+                    Name = "Lee"
+                }
+            };
+            //1、Automapper使用静态类创建映射
+
+            //ViewBag.Tips = "1、Automapper使用静态类创建映射";
+
+            //Mapper.Initialize(cfg => cfg.CreateMap<ProductEntity, ProductDTO>());
+
+            //var productDTO = Mapper.Map<ProductDTO>(productEntity);
+
+            //2、Automapper使用实例方法创建映射
+
+            //ViewBag.Tips = "2、Automapper使用实例方法创建映射";
+
+            //MapperConfiguration configuration = new MapperConfiguration(cfg => cfg.CreateMap<ProductEntity,ProductDTO>());
+
+            //var mapper = configuration.CreateMapper();
+
+            //var productDTO = mapper.Map<ProductDTO>(productEntity);
+
+            //3、使用Profie配置实现映射关系
+
+            ViewBag.Tips = "3、使用Profie配置实现映射关系";
+
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile<ATProfile>());
+
+            var productDTO = configuration.CreateMapper().Map<ProductDTO>(productEntity);
+
+            var bookviewmodel = configuration.CreateMapper().Map<BookViewModel>(book);
+
+            //return View(productDTO);
+            return View(bookviewmodel);
+
+            //return View();
         }
 
         public ActionResult About()
