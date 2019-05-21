14c81c537abb88df39ad123cb812783560579949
chenboyi081 <chenboyi081@163.com>
2019-05-21 15:52:31 +0800

    添加委托练习

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -61,10 +61,35 @@ namespace CsharpTest
             //msr.ExportMonthSalaryReport(); 
             #endregion
 
+            #region 类型转换
             //Test.类型转换.DecimalChange dc = new Test.类型转换.DecimalChange();
             //dc.DecimalToString();
-            Test.类型转换.DateTimeChange dtc = new Test.类型转换.DateTimeChange();
-            dtc.DateTimeToString();
+            //Test.类型转换.DateTimeChange dtc = new Test.类型转换.DateTimeChange();
+            //dtc.DateTimeToString(); 
+            #endregion
+
+            #region 委托练习
+            //1.0
+            //DelegateTest.GreetPeople("Liker", DelegateTest.EnglishGreeting);
+            //DelegateTest.GreetPeople("八一", DelegateTest.ChineseGreeting);
+
+            //2.0 同上
+            //GreetingDelegate delegate1;
+            //delegate1 = DelegateTest.EnglishGreeting;
+            //delegate1 += DelegateTest.ChineseGreeting;      //注意这里，第一次用的“=”，是赋值的语法；第二次，用的是“+=”，是绑定的语法。
+            //delegate1("Liker");
+
+            //3.0 既然给委托可以绑定一个方法，那么也应该有办法取消对方法的绑定，很容易想到，这个语法是“-=”：
+            GreetingDelegate delegate2 = new GreetingDelegate(DelegateTest.EnglishGreeting);
+            delegate2 += DelegateTest.ChineseGreeting;
+            DelegateTest.GreetPeople("Liker", delegate2);
+            Console.WriteLine();
+
+            delegate2 -= DelegateTest.EnglishGreeting;
+            DelegateTest.GreetPeople("李志中", delegate2);
+
+            #endregion
+
             Console.ReadKey();
         }
 
---
+++ ConsoleApplication1/Test/DelegateTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    
+    public delegate void GreetingDelegate(string name);
+
+    /// <summary>
+    /// 委托学习
+    /// C#中的委托:https://www.cnblogs.com/hushzhang/p/5901052.html
+    /// </summary>
+    public class DelegateTest
+    {
+        //我们现在对委托做一个总结：委托是一个类，它定义了方法的类型，使得可以将方法当作另一个方法的参数来进行传递，这种将方法动态地赋给参数的做法，
+        //可以避免在程序中大量使用If … Else(Switch)语句，同时使得程序具有更好的可扩展性。
+
+
+        public static void EnglishGreeting(string name)
+        {
+            Console.WriteLine("Good Morning, " + name);
+        }
+
+        public static void ChineseGreeting(string name)
+        {
+            Console.WriteLine("早上好, " + name);
+        }
+
+        public static void GreetPeople(string name, GreetingDelegate MakeGreeting)
+        {
+            MakeGreeting(name);
+        }
+
+
+    }
+}
