e134a11821a2dc349fd4f235a8828a02796c8f9b
chenboyi081 <chenboyi081@163.com>
2019-06-04 16:49:43 +0800

    VS中使用类图设计

---
+++ ConsoleApplication1/AutoMapperWeb/Models/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace AutoMapperWeb
+{
+    public class Class1
+    {
+        public DateTime Property
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
