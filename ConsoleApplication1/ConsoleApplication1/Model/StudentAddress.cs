d6d0558f6bee3803b2db62052bbfa21dfab13375
chenboyi081 <chenboyi081@163.com>
2019-06-10 11:15:32 +0800

    添加类图项目

---
+++ ConsoleApplication1/ConsoleApplication1/Model/StudentAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTest.Model
+{
+    public class StudentAddress
+    {
+        //[Key, ForeignKey("Student")]  在Fluent API 配置中设置
+        public int StudentId { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public int Zipcode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
