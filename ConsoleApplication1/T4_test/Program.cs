1add14d766cbf262b2ed46ddde556fc2acfc17dd
chenboyi081 <chenboyi081@163.com>
2019-06-14 11:07:39 +0800

    添加运行时文本模板继承

--- ConsoleApplication1/T4_test/Program.cs
+++ ConsoleApplication1/T4_test/Program.cs
@@ -12,11 +12,17 @@ namespace T4_test
     {
         static void Main(string[] args)
         {
+            #region C06  运行时文本模板-继承模式
+            C06RTTemplateSon t1 = new C06RTTemplateSon();
+            string result = t1.TransformText();
+            Console.WriteLine(result); 
+            #endregion
+
             #region C05RuntimeTextTemplate 运行时文字模板
-            List<MyData> list = new List<MyData>() { new MyData { Value =1,Name="春天在哪里啊？"}, new MyData { Value = 2, Name = "好一朵美丽的茉莉花！" } };
-            C05RuntimeTextTemplate rtt = new C05RuntimeTextTemplate(list);
-            string rrtContent = rtt.TransformText();
-            System.IO.File.WriteAllText("ouputPage.html", rrtContent); 
+            //List<MyData> list = new List<MyData>() { new MyData { Value =1,Name="春天在哪里啊？"}, new MyData { Value = 2, Name = "好一朵美丽的茉莉花！" } };
+            //C05RuntimeTextTemplate rtt = new C05RuntimeTextTemplate(list);
+            //string rrtContent = rtt.TransformText();
+            //System.IO.File.WriteAllText("ouputPage.html", rrtContent); 
             #endregion
 
             #region C04CodeGeneratorTest 生成类从XML中
